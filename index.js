7a023e28846c219168ba302d65f04734a09b0b01
Florian DUPUIS <florian@wecrea.com>
2023-04-04 06:15:45 +0200

    Add some tests

--- index.js
+++ index.js
@@ -67,8 +67,6 @@ async function run() {
     //bodyStorage.append('store', true);
     bodyStorage.append('versioningToken', dataInit.body.token);
 
-    console.log(bodyStorage);
-
     const resultUpload = await fetch(CloudFactoryApiHost + 'versioning/upload', {
       method: 'POST',
       body: bodyStorage,
@@ -78,10 +76,16 @@ async function run() {
     });
     const dataUpload = await resultUpload.json();
 
+    if (dataUpload.success === false) {
+      core.setFailed(dataUpload.message);
+      return false;
+    }
+
     console.info(`Ziplo Action | Upload finished successfully`);
 
     const finalResult = JSON.stringify(dataUpload, undefined, 2)
     console.log(`The final Result: ${finalResult}`);
+
     core.setOutput("consignment-token", dataInit.body.token);
 
     console.info("Ziplo Action | End of action");
