f81b7e923df7e1d17324ff5af068dd6dd641c783
Florian DUPUIS <florian@wecrea.com>
2023-05-24 11:22:40 +0200

    Add some debug messages

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ async function run() {
     console.info(`Ziplo Action | Call Ziplo to init upload`);
 
     const resultInit = await apiService.reservation(organizationToken, projectToken);
-
     const dataInit = await resultInit.json();
 
     if (dataInit.success === false) {
@@ -42,12 +41,16 @@ async function run() {
     formdata.append('file', fileBuffer);
 
     const resultUpload = await apiService.upload(dataInit.body.uuid, formdata);
+    console.info(`Ziplo Action | File uploaded successfully`);
     const dataUpload = await resultUpload.json();
+    console.info(dataUpload);
 
     if (dataUpload.success === false) {
       core.setFailed(dataUpload.message);
       return false;
     }
+
+    console.info(`Ziplo Action | Validate consignment`);
 
     const resultConsignment = await apiService.consignment(organizationToken, projectToken, dataInit._id);
     const dataConsignment = await resultConsignment.json();
@@ -57,11 +60,8 @@ async function run() {
       return false;
     }
 
-    console.info(`Ziplo Action | Upload finished successfully`);
-
-    core.setOutput("consignment_token", dataInit.body.token);
-
-    console.info("Ziplo Action | End of action");
+    console.info(`Ziplo Action | Uploaded and validated successfully`);
+    console.info("Ziplo Action | End of action, code protected !");
   } catch (error) {
     core.setFailed(error.message);
   }
